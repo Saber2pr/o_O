65db326c5a9e7d77b625851b8c4c626e28e0e027
saber2pr <saber2pr@gmail.com>
2020-01-11 23:42:00 +0000

    update

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
  * @Author: saber2pr
  * @Date: 2020-01-11 19:16:52
  * @Last Modified by: saber2pr
- * @Last Modified time: 2020-01-11 19:57:04
+ * @Last Modified time: 2020-01-11 23:40:58
  */
 const actress = [
+  "小鸟游-怜",
   "小鸟游-怜",
   "小芦-睦海",
   "绀堂地-衡理",
@@ -12,6 +13,7 @@ const actress = [
   "妮娜-K",
   "二阶堂-嗣",
   "文嘉",
+  "天音",
   "二玉子-舞",
   "日向-凛",
   "一条-绫香",
@@ -20,10 +22,12 @@ const actress = [
   "文岛",
   "yoyo",
   "王红花",
+  "忍蛙",
+  "万场-盟华",
   "兼志谷-星",
   "薰子",
   "四谷裕美",
-  "寡妇",
+  "深沙希",
   "黑胶佬",
   "芹菜",
   "肥结",
@@ -32,41 +36,42 @@ const actress = [
   "稚娜",
   "爱花",
   "涛哥",
-  "女主",
-  "吾妻枫"
+  "夜露",
+  "吾妻枫",
+  "丽塔",
+  "杏奈"
 ]
 
+const MAX_REC = 50
 const items = actress.map(name => ({ name }))
+const POOL = [
+  {
+    level: 4,
+    value: 0.01,
+    items
+  },
+  {
+    level: 3,
+    value: 0.1,
+    items
+  },
+  {
+    level: 2,
+    value: 0.5,
+    items
+  },
+  {
+    level: 1,
+    value: 1,
+    items
+  }
+]
 
-const config = {
-  pool: [
-    {
-      level: 4,
-      value: 0.01,
-      items
-    },
-    {
-      level: 3,
-      value: 0.1,
-      items
-    },
-    {
-      level: 2,
-      value: 0.5,
-      items
-    },
-    {
-      level: 1,
-      value: 1,
-      items
-    }
-  ]
-}
-
-const ToolBox = ({ openset }) => {
+const ToolBox = ({ openset, openrec }) => {
   return jsx`
     <nav class="toolbox">
       <button class="btn float-left" onclick=${openset}>设置</button>
+      <button class="btn float-right" onclick=${openrec}>抽卡记录</button>
     </nav>`
 }
 
@@ -89,9 +94,8 @@ const Run = ({ exec }) => {
     </div>`
 }
 
-const ValueInput = ({ n }) => {
-  const pool = config.pool
-  const current = pool[pool.length - n]
+const ValueInput = ({ list, n }) => {
+  const current = list[list.length - n]
   return jsx`
     <${jsx.frag}>
       ${n}星概率：<input value=${current.value} oninput=${event => {
@@ -100,7 +104,7 @@ const ValueInput = ({ n }) => {
     </${jsx.frag}>`
 }
 
-const Setting = ({ closeset }) => {
+const Setting = ({ list, closeset }) => {
   return jsx`
     <div class="setting">
       <nav class="toolbox">
@@ -110,15 +114,33 @@ const Setting = ({ closeset }) => {
         <div class="box">
           <div class="content">设置</div>
           <ul>
-            <li><${ValueInput} n=${4} /></li>
-            <li><${ValueInput} n=${3} /></li>
-            <li><${ValueInput} n=${2} /></li>
-            <li>1星概率：<input disabled value=${config.pool[3].value}/></li>
+            <li><${ValueInput} list=${list} n=${4} /></li>
+            <li><${ValueInput} list=${list} n=${3} /></li>
+            <li><${ValueInput} list=${list} n=${2} /></li>
+            <li>1星概率：<input disabled value=${list[3].value}/></li>
           </ul>
         </div>
       </div>
       <div class="setting-confirm">
         <button class="btn" onclick=${closeset}>确定</button>
+      </div>
+    </div>`
+}
+
+const Recording = ({ ref, closerec }) => {
+  return jsx`
+    <div class="recording">
+      <nav class="toolbox">
+        <button class="btn float-right" onclick=${closerec}>返回</button>
+      </nav>
+      <div class="recording-content">
+        <div class="box">
+          <div class="content">抽卡记录</div>
+          <table ref=${ref}></table>
+        </div>
+      </div>
+      <div class="recording-confirm">
+        最多只显示${MAX_REC}条记录
       </div>
     </div>`
 }
@@ -140,10 +162,18 @@ const PoolHelper = {
       })
 }
 
+const cleanRec = (rec, max) => {
+  while (rec.length > max) {
+    rec.pop()
+  }
+}
+
 const useSelect = list => {
   const ref = {}
+  const rec_ref = {}
+  const record = []
 
-  const setState = items =>
+  const setState = items => {
     jsx.render(
       jsx`<${jsx.frag}>${items.map(
         ({ name, level }) =>
@@ -152,29 +182,50 @@ const useSelect = list => {
       ref.current
     )
 
-  const select = n => setState(PoolHelper.compute(list, n))
+    record.unshift(...items)
+    cleanRec(record, MAX_REC)
 
-  return [ref, select]
+    jsx.render(
+      jsx`<${jsx.frag}>${record.map(
+        ({ name, level }) =>
+          jsx`<tr class=${"level" + level} style=${{
+            color: "#a93c3c"
+          }}><th align="left" style=${{ padding: "0 0.5rem" }}>${"★".repeat(
+            level
+          )}</th><td style=${{
+            padding: "0 0.5rem"
+          }}>${name}</td></tr>`
+      )}<${jsx.frag}/>`,
+      rec_ref.current
+    )
+  }
+
+  const select = n => setState(PoolHelper.compute(list, n))
+  return [ref, rec_ref, select]
 }
 
 const Layout = ({ list }) => {
-  const [ref, select] = useSelect(list)
+  const [ref, rec_ref, select] = useSelect(list)
 
   const topRef = {}
+  const clearTop = () => {
+    topRef.current.className = ""
+  }
 
   const openset = () => {
     topRef.current.className = "show-set"
   }
 
-  const closeset = () => {
-    topRef.current.className = ""
+  const openrec = () => {
+    topRef.current.className = "show-rec"
   }
 
   return jsx`
     <div ref=${topRef}>
-      <${Setting} closeset=${closeset}/>
+      <${Setting} list=${list} closeset=${clearTop}/>
+      <${Recording} ref=${rec_ref} closerec=${clearTop}/>
       <div class="layout">
-        <${ToolBox} openset=${openset}/>
+        <${ToolBox} openset=${openset} openrec=${openrec}/>
         <div class="box">
           <div class="header">抽卡模拟器(检验欧气的时候到了qwq！)</div>
           <${Result} ref=${ref}/>
@@ -187,7 +238,4 @@ const Layout = ({ list }) => {
     </div>`
 }
 
-jsx.render(
-  jsx`<${Layout} list=${config.pool}/>`,
-  document.getElementById("root")
-)
+jsx.render(jsx`<${Layout} list=${POOL}/>`, document.getElementById("root"))
